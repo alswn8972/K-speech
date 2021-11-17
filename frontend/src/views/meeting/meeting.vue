2359287ebc66feebb16302bff5b51ca661b5679e
bongdaehyun <pkjeogus@gmail.com>
2021-11-17 14:12:41 +0900

    Feat : openvidu setting

---
+++ frontend/src/views/meeting/meeting.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="meeting">
+    <web-cam :location="location"/>
+  </div> 
+</template>
+
+<script>
+//import Nav from "@/common/Nav/Nav";
+import WebCam from '@/common/WebCam/WebCam';
+
+export default {
+  name : "meeting",
+  components : {
+      //Nav,
+      WebCam,
+  },
+  data() {
+    return {
+      location : "meeting",
+    }
+  },
+  created(){
+    if(this.$store.getters.getUser === null){
+      this.$router.push('/login');
+    }
+    // else if(this.$store.getters.getSchoolId === null) {
+    //   this.$router.push({
+    //     name: 'Unity',
+    //   })
+    // }   
+  }
+}
+</script>
+
+<style scoped>
+#meeting{
+  display: flex;
+  height : 100vh;
+  overflow: hidden;
+  flex-direction: column;
+}
+</style>
