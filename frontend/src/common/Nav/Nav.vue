e7f9b7d054674e4996cfac5201830918d2aa22e8
bongdaehyun <pkjeogus@gmail.com>
2021-11-18 11:09:00 +0900

    Fix : Bug & Unity css

--- frontend/src/common/Nav/Nav.vue
+++ frontend/src/common/Nav/Nav.vue
@@ -4,58 +4,6 @@
       <div class="logo">
         <img src="@/../public/Image/kspeech_logo.png" width="200px" height="120px" id="icon" v-if="this.$route.name !== 'About'"/>
       </div>
-      <!-- <div id="user-container">
-        <div v-if="user!==null" id="user-img">
-          <img v-if="user.userImg===''" src="@/../public/Image/user_profile.png" id="user-icon" />
-          <img v-else :src="user.userImg" id="user-icon" />
-          <div id="user-level">
-            <img
-              v-if="user.userLank==='bronze1'"
-              src="@/../public/Image/level/level_bronze1.png"
-              id="level-icon"
-            />
-            <img
-              v-else-if="user.userLank==='bronze2'"
-              src="@/../public/Image/level/level_bronze2.png"
-              id="level-icon"
-            />
-            <img
-              v-else-if="user.userLank==='bronze3'"
-              src="@/../public/Image/level/level_bronze3.png"
-              id="level-icon"
-            />
-            <img
-              v-else-if="user.userLank==='silver1'"
-              src="@/../public/Image/level/level_silver1.png"
-              id="level-icon"
-            />
-            <img
-              v-else-if="user.userLank==='silver2'"
-              src="@/../public/Image/level/level_silver2.png"
-              id="level-icon"
-            />
-            <img
-              v-else-if="user.userLank==='silver3'"
-              src="@/../public/Image/level/level_silver3.png"
-              id="level-icon"
-            />
-            <img
-              v-else-if="user.userLank==='gold1'"
-              src="@/../public/Image/level/level_gold1.png"
-              id="level-icon"
-            />
-            <img
-              v-else-if="user.userLank==='gold2'"
-              src="@/../public/Image/level/level_gold2.png"
-              id="level-icon"
-            />
-            <img
-              v-else-if="user.userLank==='gold3'"
-              src="@/../public/Image/level/level_gold3.png"
-              id="level-icon"
-            />
-          </div>
-        </div> -->
         <div id="menu-container" v-if="this.user === null">
             <div @click="handleAbout" style="margin-right:20px;" class="pixel2">About</div>
             <div @click="handleLogin" style="margin-right:20px;" class="pixel2">로그인</div>
@@ -106,7 +54,7 @@ export default {
     },
     handleLogout(){
       this.$store.commit('logout');
-      window.location.href="http://localhost:8000/"
+      window.location.href="https://k5d104.p.ssafy.io/"
     },
     handleAbout(){
         this.$router.push('/').catch(()=>{});;
